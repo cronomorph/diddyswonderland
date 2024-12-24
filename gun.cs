bc5b9f77be07b302d8bd5706bbd99f10e9e5a6f3
Keegan Stanislav <ScottLoganMaxwell@gmail.com>
2024-12-23 19:52:59 -0500

    Update gun.cs

--- gun.cs
+++ gun.cs
@@ -1,1 +1,13 @@
+using UnityEngine;
+using Photon.Pun;
 
+public class gun : MonoBehaviourPunCallbacks 
+  [SerializeField] private GameObject bullet;
+
+    void Update() {
+      if (OnMouseDown(0)) {
+        PhotonNetwork.Instintiate(bullet.name, base.position, base.rotation);
+      }
+    }
+  
+}
